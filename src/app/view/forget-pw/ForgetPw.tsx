31313baed7ec5a8fe07c98b1a40b910da5decdc7
vietcoder1998 <duyviet2841998@gmail.con>
2019-12-16 22:38:53 +0700

    - update reset password

--- src/app/view/forget-pw/ForgetPw.tsx
+++ src/app/view/forget-pw/ForgetPw.tsx
@@ -8,10 +8,11 @@ import { _requestToServer } from '../../../services/exec';
 import { POST } from '../../../common/const/method';
 import { M_S } from '../../../services/api/public.api';
 import { noInfoHeader } from '../../../services/auth';
-import {     OAUTH2_HOST } from '../../../environment/dev';
+import { OAUTH2_HOST } from '../../../environment/dev';
 
 interface IFgpws {
     email?: string
+    loading?: boolean;
 }
 
 interface IFgpwp {
@@ -22,13 +23,15 @@ class ForgotPw extends Component<IFgpwp, IFgpws>{
     constructor(props) {
         super(props);
         this.state = {
-            email: ''
+            email: '',
+            loading: false,
         }
     }
 
 
     createRequest = () => {
         let { email } = this.state;
+        this.setState({ loading: true });
         _requestToServer(
             POST,
             M_S,
@@ -39,16 +42,17 @@ class ForgotPw extends Component<IFgpwp, IFgpws>{
             noInfoHeader,
             OAUTH2_HOST,
             true
-        )
+        ).finally(() => {
+            this.setState({ loading: false })
+        })
     }
 
 
     render() {
-        let { email } = this.state;
+        let { email, loading } = this.state;
         return (
             <>
                 <Header />
-                {/* <form> */}
                 <Row>
                     <Col xs={4} sm={0} md={6} xl={8} lg={7} ></Col>
                     <Col xs={16} sm={24} md={12} xl={8} lg={10} >
@@ -68,11 +72,28 @@ class ForgotPw extends Component<IFgpwp, IFgpws>{
                                             </Tooltip>
                                         }
                                         value={email}
-                                        onChange={(event: any) => this.setState({ email: event.target.value })} type='text'
+                                        onChange={(event) => {
+                                            this.setState({ email: event.target.value })
+                                        }
+                                        }
+                                        onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+                                            if (event.keyCode === 13) {
+                                                this.createRequest()
+                                            }
+                                        }}
+                                        type='text'
                                     />
                                 </p>
                                 <p>
-                                    <Button type='primary' onClick={() => this.createRequest()} block>Gửi</Button>
+                                    <Button
+                                        type='primary'
+                                        onClick={
+                                            () => this.createRequest()
+                                        }
+                                        block
+                                    >
+                                        {loading ? <Icon type={'loading'} /> : 'Gửi'}
+                                    </Button>
                                 </p>
                                 <p className='a_c'>
                                     Quay trở lại? <a href='/register' style={{ color: 'red' }}>Đăng kí</a>
